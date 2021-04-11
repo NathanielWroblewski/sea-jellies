11b725c05506cbcb2d490172b23c0c117215fd20
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2021-04-11 12:06:26 -0400

    Add title animation

--- public/javascripts/constants/dimensions.js
+++ public/javascripts/constants/dimensions.js
@@ -1,7 +1,7 @@
 export const τ = 2 * Math.PI
 
-export const FRAMES = 200
+export const FRAMES = 40
 
-export const LERP_COUNT = 250
+export const LERP_COUNT = 240
 
 export const DELAY_FACTOR = 2.0
